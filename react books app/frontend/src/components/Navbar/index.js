b57553e5744b7b30b5dd88effd0c2d005574e31c
journo10 <80216907+journo10@users.noreply.github.com>
2022-02-27 22:49:58 +0300

    react book app

---
+++ react books app/frontend/src/components/Navbar/index.js
@@ -0,0 +1,30 @@
+import { Link, Outlet } from 'react-router-dom'
+import React from 'react'
+
+const Navbar = () => {
+    return (
+        <div>
+            <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div className="container-fluid">
+                    <Link to="/books" className="navbar-brand" >Book App</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li className="nav-item">
+                                <Link to="/books" className="nav-link active" aria-current="page" >Books</Link>
+                            </li>
+                            <li className="nav-item">
+                                <Link to="/add-book" className="nav-link">Add Book</Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <Outlet />
+        </div>
+    )
+}
+
+export default Navbar
